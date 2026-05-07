2ba62edd522f21f5fd979182ed365eac24cb88e0
Teb0so <joaopedrostcunha@gmail.com>
2026-05-07 16:10:17 -0300

    3px border in dmenu

--- dmenu/config.def.h
+++ dmenu/config.def.h
@@ -26,4 +26,4 @@ static unsigned int lines      = 0;
 static const char worddelimiters[] = " ";
 
 /* Size of the window border */
-static unsigned int border_width = 2;
+static unsigned int border_width = 3;
